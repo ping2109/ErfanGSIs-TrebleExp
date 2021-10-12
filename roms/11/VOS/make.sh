44b35b47c5405aa183e590e2fa39d5f9cb933ce0
pingmado <75196272+ping2109@users.noreply.github.com>
2021-10-12 16:48:07 +0700

    Update make.sh

--- roms/11/VOS/make.sh
+++ roms/11/VOS/make.sh
@@ -3,6 +3,10 @@
 systempath=$1
 thispath=`cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`
 
+echo "qemu.hw.mainkeys=1" >> $1/build.prop
+echo "persist.bluetooth.bluetooth_audio_hal.disabled=true " >> $1/build.prop
+echo "ro.bluetooth.library_name=libbluetooth_qti.so" >> $1/build.prop
+
 # drop VinServiceApp (disable system warning)
 rm -rf $1/priv-app/VinServiceApp
 rm -rf $1/priv-app/VDefense
